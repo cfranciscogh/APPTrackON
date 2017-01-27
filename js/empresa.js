16165308746b4dcd0f318f263f801c64e5a4e62d
Francisco <franz_568@hotmail.com>
2017-01-27 17:49:53 -0500

    ds
    a

--- js/empresa.js
+++ js/empresa.js
@@ -3,6 +3,7 @@ $(document).ready(function(e) {
 	
 	$("#listEmpresas li a").eq(0).attr("href","panel.html?idChofer="+$.QueryString["idChofer"]+"&empresa=SODIMA");
 	$("#listEmpresas li a").eq(1).attr("href","panel.html?idChofer="+$.QueryString["idChofer"]+"&empresa=MAESTR");
+	$("#listEmpresas li a").eq(2).attr("href","panel.html?idChofer="+$.QueryString["idChofer"]+"&empresa=SOLTRA");
 });
 
    
